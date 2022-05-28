e1a31d5c8bdf6b88b7c3de3b01fcdaa32d96a6bd
Quoc Duong <Quocduong.th10b.mta@gmail.com>
2022-05-28 20:27:37 +0700

    Update code

--- function/ServiceBusQueueTrigger/__init__.py
+++ function/ServiceBusQueueTrigger/__init__.py
@@ -9,7 +9,7 @@ from sendgrid.helpers.mail import Mail
 
 def main(msg: func.ServiceBusMessage):
 
-    notification_id = int(msg.get_body().decode('utf-8'))
+    notification_id = msg.get_body().decode('utf-8')
     logging.info(
         'Python ServiceBus queue trigger processed message: %s', notification_id)
 
